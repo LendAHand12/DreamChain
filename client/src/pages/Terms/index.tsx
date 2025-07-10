ccceef8826031e37498ca2d778930149a5791e87
AnhKiet1010 <letrananhkiet1010@gmail.com>
2025-07-10 23:52:28 +0700

    done content

--- client/src/pages/Terms/index.tsx
+++ client/src/pages/Terms/index.tsx
@@ -6,94 +6,131 @@ const TermsPage: React.FC = () => {
     <DefaultLayout>
       <div className="w-full md:p-24 py-24 px-10 space-y-10">
         <div>
-          <h1 className="text-3xl font-bold mb-4">
-            NoExcuseChallenge TERMS & CONDITIONS
-          </h1>
-          <p className="">Effective Date: November 12, 2024</p>
+          <h1 className="text-3xl font-bold mb-4">TERMS & CONDITIONS</h1>
+          <p className="text-xl font-bold">Dreamchain</p>
+          <p>Terms, Conditions, and Community Guidelines</p>
         </div>
         <div className="space-y-8">
           <div className="space-y-4">
-            <b className="text-xl font-bold">1. Acceptance of Terms</b>
-            <ul className="grid list-disc gap-4 md:gap-8">
+            <b className="text-xl font-bold">1. Platform Policy</b>
+            <ul className="grid list-disc gap-4">
               <li className="ml-6">
-                By accessing or using the NoExcuseChallenge website
-                noexcuse.live services, or products MTD phone protection and our
-                management program NoExcuseChallenge , you (“User” or “you”)
-                agree to be bound by these Terms & Conditions . If you do not
-                agree to all of the Terms, please do not use the Services.
+                We provide a platform for peer-to-peer contributions and
+                community-driven support. All decisions are made independently
+                by participants.
+              </li>
+              <li className="ml-6">
+                Ameritec IPS, a U.S. corporation, proudly provides this
+                platform, operating under U.S. regulations.
               </li>
             </ul>
           </div>
         </div>
         <div className="space-y-8">
           <div className="space-y-4">
-            <b className="text-xl font-bold">2. Eligibility</b>
-            <ul className="grid list-disc gap-4 md:gap-8">
+            <b className="text-xl font-bold">2. General Terms</b>
+            <ul className="grid list-disc gap-4">
               <li className="ml-6">
-                You must be at least the age of majority in your jurisdiction to
-                use the Services. By using the Site, you represent and warrant
-                that you have the legal capacity to enter into a binding
-                agreement and that all information you provide is accurate and
-                truthful
+                One ID Per Person: Duplicate or fake accounts are strictly
+                prohibited. Violations result in immediate termination.
+              </li>
+              <li className="ml-6">
+                No Negative Talk: Speaking negatively about the company,
+                program, or community is prohibited. Violators will receive a
+                warning, suspension, or termination.
+              </li>
+              <li className="ml-6">
+                No System Abuse: Manipulation, cheating, or exploiting the
+                system will lead to immediate account termination.
+              </li>
+              <li className="ml-6">
+                No Cross-Recruiting: Recruiting (or Ameritec IPS) members into
+                other programs or MLMs is prohibited. Violation = Warning →
+                Permanent Ban.
+              </li>
+              <li className="ml-6">
+                Stay Active & Committed: Members must actively participate to
+                maintain their benefits. Extended inactivity may result in
+                benefit loss or removal.
+              </li>
+              <li className="ml-6">
+                Respect & Integrity: Treat all members fairly and
+                professionally.
+              </li>
+              <li className="ml-6">
+                No Unauthorized Transfers: Selling, gifting, or transferring
+                accounts without written approval is forbidden.
+              </li>
+              <li className="ml-6">
+                Compliance: All members must follow DreamChain’s official
+                policies and guidelines. Non-compliance may lead to account
+                restrictions or termination.
               </li>
             </ul>
           </div>
         </div>
         <div className="space-y-8">
           <div className="space-y-4">
-            <b className="text-xl font-bold"> 3. Changes to Terms</b>
-            <ul className="grid list-disc gap-4 md:gap-8">
+            <b className="text-xl font-bold"> 3. Participation Requirements</b>
+            <ul className="grid list-disc gap-4">
               <li className="ml-6">
-                NoExcuseChallenge reserves the right, at our discretion, to
-                modify, update, or replace these Terms at any time. Any changes
-                will be effective immediately upon posting. It is your
-                responsibility to review these Terms periodically for updates.
-                Your continued use of the Services after changes are posted
-                constitutes acceptance of the updated Terms.
+                Referrals Requirement:
+                <li className="ml-6">
+                  Members are not required to refer new members. How ever
+                  referral new member will earn reward and contribution funds
+                </li>
+              </li>
+              <li className="ml-6">
+                Binary System Clarification:
+                <li className="ml-6">DreamChain uses a binary concept.</li>
+                <li className="ml-6">
+                  Members will only benefit from the branch (left or right)
+                  where they have placed their direct referrals.
+                </li>
+                <li className="ml-6">
+                  Branches without direct referrals will not generate
+                  contribution benefits.
+                </li>
               </li>
             </ul>
           </div>
         </div>
         <div className="space-y-8">
           <div className="space-y-4">
-            <b className="text-xl font-bold">4. Intellectual Property</b>
-            <ul className="grid list-disc gap-4 md:gap-8">
-              <li className="ml-6">
-                All content, logos, designs, text, graphics, images, and other
-                materials on the Site are the property of NoExcuseChallenge or
-                its licensors and are protected by intellectual property laws.
-                You agree not to reproduce, distribute, modify, create
-                derivative works, or otherwise exploit any content without our
-                express written permission.
-              </li>
+            <b className="text-xl font-bold">4. Accounts and Eligibility</b>
+            <ul className="grid list-disc gap-4">
+              <li className="ml-6">Main accounts are eligible for promotions and rewards.</li>
+              <li className="ml-6">Sub-accounts (recycling accounts) are for system support and are not eligible for promotions or bonuses.</li>
             </ul>
           </div>
         </div>
         <div className="space-y-8">
           <div className="space-y-4">
-            <b className="text-xl font-bold">5. User Conduct</b>
-            <p className="font-medium">
-              You agree not to use the Site or Services for any unlawful purpose
-              or in a manner that violates these Terms. Prohibited activities
-              include, but are not limited to:
-            </p>
-            <ul className="grid md:grid-cols-3 gap-4 md:gap-8">
-              <li className="flex items-start gap-6">
+            <b className="text-xl font-bold">5. Important Legal Notes</b>
+            <ul className="grid list-disc gap-4">
+              <li className="ml-6">
                 <span>
-                  • Impersonating any person or entity, or falsely stating or
-                  misrepresenting your affiliation with any person or entity.
+                  DreamChain, and Ameritec IPS are not financial advisors.
                 </span>
               </li>
-              <li className="flex items-start gap-6">
+              <li className="ml-6">
                 <span>
-                  • Posting or transmitting content that is unlawful, harmful,
-                  threatening, defamatory, obscene, or otherwise objectionable.
+                  The platform facilitates giving and receiving contributions within the community in a transparent and fair manner.
                 </span>
               </li>
-              <li className="flex items-start gap-6">
+              <li className="ml-6">
                 <span>
-                  • Attempting to gain unauthorized access to our systems or the
-                  accounts of other users.
+                  Participation is voluntary. Participants are responsible for their own physical, emotional, and financial well-being.
+                </span>
+              </li>
+              <li className="ml-6">
+                <span>
+                  Organizers are not liable for any personal loss, injury, or damage.
+                </span>
+              </li>
+              <li className="ml-6">
+                <span>
+                  DreamChain or Ameritec do not guarantee or promise any interest, profit, or income.
                 </span>
               </li>
             </ul>
@@ -101,108 +138,27 @@ const TermsPage: React.FC = () => {
         </div>
         <div className="space-y-8">
           <div className="space-y-4">
-            <b className="text-xl font-bold">6. . Third-Party Links</b>
-            <ul className="grid list-disc gap-4 md:gap-8">
+            <b className="text-xl font-bold">6. . Registration and Honesty</b>
+            <ul className="grid list-disc gap-4">
               <li className="ml-6">
-                The Site may contain links to third-party websites or resources.
-                NoExcuseChallenge does not endorse and is not responsible for
-                the accuracy or content of these external sites. Your use of
-                third-party websites is at your own risk and subject to their
-                respective terms and conditions.
+              Members must register through the official platform, website, or app.
               </li>
+              <li className="ml-6">
+              Members must complete tasks and challenges with honesty and integrity.
+              </li>
+              <li className="ml-6">Membership is open worldwide unless prohibited by local laws.</li>
+              <li className="ml-6">Participants must be 18 years or older.</li>
             </ul>
           </div>
         </div>
         <div className="space-y-8">
           <div className="space-y-4">
-            <b className="text-xl font-bold">7. Disclaimers</b>
-            <ul className="grid list-disc gap-4 md:gap-8">
+            <b className="text-xl font-bold">7. Compliance and Enforcement</b>
+            <ul className="grid list-disc gap-4">
               <li className="ml-6">
-                The Services are provided on an “as is” and “as available” basis
-                without warranties of any kind, whether express or implied.
-                NoExcuseChallenge does not guarantee that the Services will be
-                secure, error-free, uninterrupted, or meet your specific
-                requirements.
+              Failure to comply with these Terms and Conditions may result in immediate disqualification.
               </li>
-            </ul>
-          </div>
-        </div>
-        <div className="space-y-8">
-          <div className="space-y-4">
-            <b className="text-xl font-bold">8. Limitation of Liability</b>
-            <ul className="grid list-disc gap-4 md:gap-8">
-              <li className="ml-6">
-                To the fullest extent permitted by law, NoExcuseChallenge and
-                its officers, directors, employees, agents, and affiliates shall
-                not be liable for any indirect, incidental, special,
-                consequential, or punitive damages arising out of or related to
-                your use of the Services.
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div className="space-y-8">
-          <div className="space-y-4">
-            <b className="text-xl font-bold">9. Indemnification</b>
-            <ul className="grid list-disc gap-4 md:gap-8">
-              <li className="ml-6">
-                You agree to indemnify, defend, and hold harmless
-                NoExcuseChallenge and its affiliates, officers, directors,
-                employees, and agents from and against any claims, liabilities,
-                damages, losses, and expenses arising from your use of the
-                Services or violation of these Terms.
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div className="space-y-8">
-          <div className="space-y-4">
-            <b className="text-xl font-bold">10. Governing Law</b>
-            <ul className="grid list-disc gap-4 md:gap-8">
-              <li className="ml-6">
-                These Terms shall be governed by and construed in accordance
-                with the laws of [Your Country/State], without regard to
-                conflict of law principles. Any disputes arising from these
-                Terms or your use of the Services shall be resolved exclusively
-                in the courts located in United State International law.
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div className="space-y-8">
-          <div className="space-y-4">
-            <b className="text-xl font-bold">11. Termination</b>
-            <ul className="grid list-disc gap-4 md:gap-8">
-              <li className="ml-6">
-                We may, at our sole discretion, suspend or terminate your access
-                to the Services at any time, with or without notice, for any
-                reason, including if we believe you have violated these Terms.
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div className="space-y-8">
-          <div className="space-y-4">
-            <b className="text-xl font-bold">12. Severability</b>
-            <ul className="grid list-disc gap-4 md:gap-8">
-              <li className="ml-6">
-                If any provision of these Terms is found to be invalid or
-                unenforceable, that provision shall be enforced to the maximum
-                extent permissible, and the remaining provisions shall remain in
-                full force and effect.
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div className="space-y-8">
-          <div className="space-y-4">
-            <b className="text-xl font-bold">13. Entire Agreement</b>
-            <ul className="grid list-disc gap-4 md:gap-8">
-              <li className="ml-6">
-                These Terms constitute the entire agreement between you and
-                NoExcuseChallenge concerning your use of the Services,
-                superseding any prior agreements.
-              </li>
+              <li className="ml-6">The organizers reserve the right to disqualify participants to preserve the integrity and spirit of the DreamChain.</li>
             </ul>
           </div>
         </div>
