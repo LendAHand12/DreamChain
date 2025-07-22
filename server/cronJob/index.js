60cc1c26997b628556b20ddeca459e228e3c1327
AnhKiet1010 <letrananhkiet1010@gmail.com>
2025-07-22 10:12:32 +0700

    done fix

--- server/cronJob/index.js
+++ server/cronJob/index.js
@@ -3,8 +3,14 @@ import moment from "moment";
 
 import User from "../models/userModel.js";
 import sendMail from "../utils/sendMail.js";
-import { sendMailGetHewePrice, sendMailUpdateLayerForAdmin } from "../utils/sendMailCustom.js";
-import { getCountAllChildren, getCountIncome } from "../controllers/userControllers.js";
+import {
+  sendMailGetHewePrice,
+  sendMailUpdateLayerForAdmin,
+} from "../utils/sendMailCustom.js";
+import {
+  getCountAllChildren,
+  getCountIncome,
+} from "../controllers/userControllers.js";
 import {
   findRootLayer,
   getTotalLevel6ToLevel10OfUser,
@@ -18,80 +24,103 @@ import Config from "../models/configModel.js";
 
 export const deleteUser24hUnPay = asyncHandler(async () => {
   const listUser = await User.find({
-    $and: [{ tier: 1 }, { countPay: 0 }, { isAdmin: false }, { status: { $ne: "DELETED" } }],
+    $and: [
+      { tier: 1 },
+      { countPay: 0 },
+      { isAdmin: false },
+      { status: { $ne: "DELETED" } },
+    ],
   });
   for (let u of listUser) {
+    console.log({ name: u.userId });
     const treeOfUser = await Tree.findOne({ userId: u._id });
-    if (treeOfUser.children.length === 2) {
-      console.log({ userId2: u.userId });
-    } else if (treeOfUser.children.length === 1) {
-      console.log({ userId1: u.userId });
-    }
-    await Tree.updateMany(
-      { refId: treeOfUser._id },
-      { $set: { refId: "64cd449ec75ae7bc7ebbab03" } }
-    );
-    let parent = await Tree.findById(treeOfUser.parentId);
-    if (parent) {
-      console.log({ processingUser: u.userId });
-      let childs = parent.children;
-      let newChilds = childs.filter((item) => {
-        if (item.toString() !== treeOfUser._id.toString()) return item;
-      });
-      parent.children = [...newChilds];
-      const updatedParent = await parent.save();
+    // console.log({ treeOfUser });
 
-      if (treeOfUser.children.length === 1 && updatedParent.children.length < 2) {
-        console.log({ TH1111111: u.userId });
-        const firstChild = await Tree.findById(treeOfUser.children[0]);
-        firstChild.parentId = updatedParent._id;
-        firstChild.refId =
-          firstChild.refId === treeOfUser._id ? "64cd449ec75ae7bc7ebbab03" : firstChild.refId;
-        await firstChild.save();
+    if (treeOfUser) {
+      let parent = await Tree.findById(treeOfUser.parentId);
+      if (parent) {
+        console.log({ processingUser: u.userId });
+        let childs = parent.children;
+        let newChilds = childs.filter((item) => {
+          if (item.toString() !== treeOfUser._id.toString()) return item;
+        });
+        parent.children = [...newChilds];
+        const updatedParent = await parent.save();
 
-        const newUpdatedParentChildren = [...updatedParent.children, firstChild._id];
-        updatedParent.children = newUpdatedParentChildren;
-        await updatedParent.save();
+        if (
+          treeOfUser.children.length === 1 &&
+          updatedParent.children.length < 2
+        ) {
+          console.log({ TH1111111: u.userId });
+          const firstChild = await Tree.findById(treeOfUser.children[0]);
+          firstChild.parentId = updatedParent._id;
+          firstChild.refId =
+            firstChild.refId === treeOfUser._id
+              ? "64cd449ec75ae7bc7ebbab03"
+              : firstChild.refId;
+          await firstChild.save();
+
+          const newUpdatedParentChildren = [
+            ...updatedParent.children,
+            firstChild._id,
+          ];
+          updatedParent.children = newUpdatedParentChildren;
+          await updatedParent.save();
+        }
+
+        if (
+          treeOfUser.children.length === 2 &&
+          updatedParent.children.length === 0
+        ) {
+          console.log({ TH22222222: u.userId });
+          const firstChild = await Tree.findById(treeOfUser.children[0]);
+          firstChild.parentId = updatedParent._id;
+          firstChild.refId === treeOfUser._id
+            ? "64cd449ec75ae7bc7ebbab03"
+            : firstChild.refId;
+          await firstChild.save();
+
+          const secondChild = await Tree.findById(treeOfUser.children[1]);
+          secondChild.parentId = updatedParent._id;
+          secondChild.refId === treeOfUser._id
+            ? "64cd449ec75ae7bc7ebbab03"
+            : secondChild.refId;
+          await secondChild.save();
+
+          const newUpdatedParentChildren = [firstChild._id, secondChild._id];
+          updatedParent.children = newUpdatedParentChildren;
+          await updatedParent.save();
+        }
+
+        if (
+          treeOfUser.children.length === 2 &&
+          updatedParent.children.length === 1
+        ) {
+          console.log({ TH333333: u.userId });
+          const firstChild = await Tree.findById(treeOfUser.children[0]);
+          const secondChild = await Tree.findById(treeOfUser.children[1]);
+          const userListString = `${firstChild.userName}, ${secondChild.userName}`;
+          await sendMailChangeSystemForUser(userListString);
+        }
+
+        u.status = "DELETED";
+        u.deletedTime = new Date();
+        u.oldParents = [parent.userId, ...u.oldParents];
+        await u.save();
+
+        await Tree.deleteOne({ userId: u._id });
+      } else {
+        u.status = "DELETED";
+        u.deletedTime = new Date();
+        u.oldParents = [parent.userId, ...u.oldParents];
+        await u.save();
+
+        await Tree.deleteOne({ userId: u._id });
       }
-
-      if (treeOfUser.children.length === 2 && updatedParent.children.length === 0) {
-        console.log({ TH22222222: u.userId });
-        const firstChild = await Tree.findById(treeOfUser.children[0]);
-        firstChild.parentId = updatedParent._id;
-        firstChild.refId === treeOfUser._id ? "64cd449ec75ae7bc7ebbab03" : firstChild.refId;
-        await firstChild.save();
-
-        const secondChild = await Tree.findById(treeOfUser.children[1]);
-        secondChild.parentId = updatedParent._id;
-        secondChild.refId === treeOfUser._id ? "64cd449ec75ae7bc7ebbab03" : secondChild.refId;
-        await secondChild.save();
-
-        const newUpdatedParentChildren = [firstChild._id, secondChild._id];
-        updatedParent.children = newUpdatedParentChildren;
-        await updatedParent.save();
-      }
-
-      if (treeOfUser.children.length === 2 && updatedParent.children.length === 1) {
-        console.log({ TH333333: u.userId });
-        const firstChild = await Tree.findById(treeOfUser.children[0]);
-        const secondChild = await Tree.findById(treeOfUser.children[1]);
-        const userListString = `${firstChild.userName}, ${secondChild.userName}`;
-        await sendMailChangeSystemForUser(userListString);
-      }
-
-      u.status = "DELETED";
-      u.deletedTime = new Date();
-      u.oldParents = [parent.userId, ...u.oldParents];
-      await u.save();
-
-      await Tree.deleteOne({ userId: u._id });
     } else {
       u.status = "DELETED";
       u.deletedTime = new Date();
-      u.oldParents = [parent.userId, ...u.oldParents];
       await u.save();
-
-      await Tree.deleteOne({ userId: u._id });
     }
   }
 });
@@ -178,7 +207,9 @@ export const areArraysEqual = (arr1, arr2) => {
 export const distributionHewe = asyncHandler(async () => {
   const listUser = await User.find({
     $and: [{ isAdmin: false }, { userId: { $ne: "Admin2" } }, { countPay: 13 }],
-  }).select("userId totalHewe availableHewe hewePerDay claimedHewe currentLayer");
+  }).select(
+    "userId totalHewe availableHewe hewePerDay claimedHewe currentLayer"
+  );
 
   for (let u of listUser) {
     try {
@@ -322,8 +353,14 @@ export const checkUserTryToTier2 = asyncHandler(async () => {
 
   for (let u of listUser) {
     try {
-      const treeOfUser = await Tree.findOne({ userId: u._id, tier: 1, isSubId: false });
-      const { countChild1, countChild2 } = await getTotalLevel6ToLevel10OfUser(treeOfUser);
+      const treeOfUser = await Tree.findOne({
+        userId: u._id,
+        tier: 1,
+        isSubId: false,
+      });
+      const { countChild1, countChild2 } = await getTotalLevel6ToLevel10OfUser(
+        treeOfUser
+      );
       if (countChild1 >= 62 && countChild2 >= 62) {
         u.tryToTier2 = "DONE";
         u.timeToTry = null;
@@ -334,7 +371,11 @@ export const checkUserTryToTier2 = asyncHandler(async () => {
           if (diffDay >= 45) {
             u.tryToTier2 = "REDO";
             u.tier = 1;
-            const treeOfUser = await Tree.findOne({ userId: u._id, tier: 2, isSubId: false });
+            const treeOfUser = await Tree.findOne({
+              userId: u._id,
+              tier: 2,
+              isSubId: false,
+            });
             treeOfUser.disable = true;
             await treeOfUser.save();
           }
